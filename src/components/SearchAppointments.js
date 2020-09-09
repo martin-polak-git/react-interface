d55a4a85ebddef0a9168f3b706401be1ede47d04
Martin Polak <martinpolak.mail@gmail.com>
2020-09-10 00:52:54 +0200

    commit all files

---
+++ src/components/SearchAppointments.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react'
+
+
+    class SearchAppointments extends Component{
+        render() {
+            return (<div className="search-appointments row justify-content-center my-4">
+                <div className="col-md-6">
+                    <div className="input-group">
+                        <input
+                            id="SearchApts"
+                            type="text"
+                            className="form-control"
+                            aria-label="Termine durchsuchen"
+                            onChange={ event => this.props.searchApts(event.target.value)}
+                        />
+                        <div className="input-group-append">
+                            <button
+                                type="button"
+                                className="btn btn-primary dropdown-toggle"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Sortieren nach: <span className="caret" />
+                            </button>
+
+                            <div className="sort-menu dropdown-menu dropdown-menu-right">
+                                <button
+                                    className={
+                                        "sort-by dropdown-item " +
+                                        (this.props.orderBy === 'petName' ? 'active' : '')
+                                    }
+                                    onClick={event => this.props.changeOrder('petName', this.props.orderDir)}
+                                    href="#">
+                                    Tiername
+                                </button>
+                                <button
+                                    className={
+                                        "sort-by dropdown-item " +
+                                        (this.props.orderBy === 'aptDate' ? 'active' : '')
+                                    }
+                                    onClick={event => this.props.changeOrder('aptDate', this.props.orderDir)}
+                                    href="#">
+                                    Datum
+                                </button>
+                                <button
+                                    className={
+                                        "sort-by dropdown-item " +
+                                        (this.props.orderBy === 'ownerName' ? 'active' : '')
+                                    }
+                                    onClick={event => this.props.changeOrder('ownerName', this.props.orderDir)}
+                                    href="#">
+                                    Besitzer
+                                </button>
+                                <div role="separator" className="dropdown-divider" />
+                                <button
+                                    className={
+                                        "sort-by dropdown-item " +
+                                        (this.props.orderDir === 'asc' ? 'active' : '')
+                                    }
+                                    onClick={event => this.props.changeOrder(this.props.orderBy,'asc')}
+                                    href="#">
+                                    aufsteigend
+                                </button>
+                                <button
+                                    className={
+                                        "sort-by dropdown-item " +
+                                        (this.props.orderDir === 'desc' ? 'active' : '')
+                                    }
+                                    onClick={event => this.props.changeOrder(this.props.orderBy,'desc')}
+                                    href="#">
+                                    absteigend
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>)
+        }
+    }
+
+
+export default SearchAppointments;
